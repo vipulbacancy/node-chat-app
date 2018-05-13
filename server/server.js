a1172576bdbd4856bfd475726bc4a930954243a3
vipzmangukiya <vbmm20@gmail.com>
2018-05-13 19:05:13 +0530

    Add Connect and Disconnet

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
-const path 		= require('path');
-const publicPath = path.join(__dirname,'../public/');
-const express	= require('express');
+const path 			= require('path');
+const http 			= require('http');
+const express		= require('express');
+const socketIO		= require('socket.io');
 
-var app = express();
+const publicPath 	= path.join(__dirname,'../public/');
+const port 			= process.env.PORT || 3000;
+
+var app 	= express();
+var server 	= http.createServer(app); 
+var io 		= socketIO(server); 
 
 app.use(express.static(publicPath));
 
-app.listen(3000,()=>{
-	console.log('Vipul MAngukiya');
+io.on('connection',(socket)=>{
+	console.log('NEw USer Connected');
+	socket.on('disconnect',()=> {
+		console.log('USer was disconnected ');
+	});
+});
+
+server.listen(port,()=>{
+	console.log(`Running Server at ${port}`);
 })
 console.log(__dirname+'../public/');
